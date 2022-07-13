47bf32947342734784c82d50812b422ab078b568
vicmanuelr <43187779+vicmanuelr@users.noreply.github.com>
2022-07-13 17:17:20 -0600

    Step 1 Create Encrytp funciont
    Takes test and shift arguments.

---
+++ main.py
@@ -0,0 +1,29 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
+# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount
+#  and print the encrypted text. e.g. plain_text = "hello" shift = 5 cipher_text = "mjqqt" print output: "The encoded
+#  text is mjqqt"
+    encrypted_text = ""
+    for letter in text:
+        new_index = alphabet.index(letter) + shift
+        if new_index > len(alphabet) - 1:
+            new_index = new_index - len(alphabet)
+        encrypted_text += alphabet[new_index]
+    return encrypted_text
+
+# HINT: How do you get the index of an item in a list:
+# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+
+print(encrypt(text,shift))
