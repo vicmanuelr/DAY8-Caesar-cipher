a3fa80ad9aebab8f8032a2e73cc19e3ef0383db9
vicmanuelr <43187779+vicmanuelr@users.noreply.github.com>
2022-07-13 18:01:34 -0600

    Step 2 - Staging
    Create decrypt function and check user selection to select correct function.

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount
-#  and print the encrypted text. e.g. plain_text = "hello" shift = 5 cipher_text = "mjqqt" print output: "The encoded
-#  text is mjqqt"
+
+def encrypt(plain_text, shift_amount):
     encrypted_text = ""
-    for letter in text:
-        new_index = alphabet.index(letter) + shift
+    for letter in plain_text:
+        new_index = alphabet.index(letter) + shift_amount
         if new_index > len(alphabet) - 1:
             new_index = new_index - len(alphabet)
         encrypted_text += alphabet[new_index]
-    return encrypted_text
+    print(f"The encoded text is {encrypted_text}")
 
-# HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+# TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+# TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift
+#  amount and print the decrypted text. e.g. cipher_text = "mjqqt" shift = 5 plain_text = "hello" print output: "The
+#  decoded text is hello"
 
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
-print(encrypt(text,shift))
+# TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call
+#  the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND*
+#  decrypt a message.
