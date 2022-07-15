fc749e22e4360cf0ea2cafc547ca44ff4ae844a4
vicmanuelr <43187779+vicmanuelr@users.noreply.github.com>
2022-07-14 22:08:25 -0600

    final update user choice to continue added
    Added question if user would like to contine y or no, and also error check if user selects something different.

--- main.py
+++ main.py
@@ -5,42 +5,48 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+user_continue = True
+while user_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-if shift > len(alphabet):
-    shift = shift % len(alphabet)
-
-
-def caesar(text_entered, text_movement, decrypt_method):
-    encrypted_text = ""
-    if decrypt_method == "encode":
-        for char in text_entered:
-            if char in alphabet:
-                new_index = alphabet.index(char) + text_movement
-                if new_index > len(alphabet) - 1:
-                    new_index = new_index - len(alphabet)
-                encrypted_text += alphabet[new_index]
-            else:
-                encrypted_text += char
-        print(f"The encoded text is {encrypted_text}")
-    elif decrypt_method == "decode":
-        for char in text_entered:
-            if char in alphabet:
-                new_index = alphabet.index(char) - text_movement
-                if new_index < 0:
-                    new_index = new_index + len(alphabet)
-                encrypted_text += alphabet[new_index]
-            else:
-                encrypted_text += char
-        print(f"The decrypted text is {encrypted_text}")
+    if shift > len(alphabet):
+        shift = shift % len(alphabet)
 
 
-caesar(text_entered=text, text_movement=shift, decrypt_method=direction)
+    def caesar(text_entered, text_movement, decrypt_method):
+        encrypted_text = ""
+        if decrypt_method == "encode":
+            for char in text_entered:
+                if char in alphabet:
+                    new_index = alphabet.index(char) + text_movement
+                    if new_index > len(alphabet) - 1:
+                        new_index = new_index - len(alphabet)
+                    encrypted_text += alphabet[new_index]
+                else:
+                    encrypted_text += char
+            print(f"The encoded text is -->{encrypted_text}<--")
+        elif decrypt_method == "decode":
+            for char in text_entered:
+                if char in alphabet:
+                    new_index = alphabet.index(char) - text_movement
+                    if new_index < 0:
+                        new_index = new_index + len(alphabet)
+                    encrypted_text += alphabet[new_index]
+                else:
+                    encrypted_text += char
+            print(f"The decrypted text is -->{encrypted_text}<--")
 
 
-# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+    caesar(text_entered=text, text_movement=shift, decrypt_method=direction)
+    choice_wrong = True
+    while choice_wrong:
+        user_choice = input("Would you like to use program again? Type 'y' or 'n'? ")
+        if user_choice == "n":
+            user_continue = False
+            choice_wrong = False
+        elif user_choice == "y":
+            choice_wrong = False
+        else:
+            print("Please select 'y' or 'n' ")
