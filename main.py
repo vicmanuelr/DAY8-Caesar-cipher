6ae5c1cfe6c1f2d7ce9961dd4c8383ba80d2e7ff
vicmanuelr <43187779+vicmanuelr@users.noreply.github.com>
2022-07-14 21:34:32 -0600

    step 4 staging
    Adding art.py, fix bugs (more than alphabet chars input, what happens if user use number/symbol/space, restart cipher program after asking a user)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
+
+print(logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -49,3 +53,20 @@ def caesar(text_entered, text_movement, decrypt_method):
 
 
 caesar(text_entered=text, text_movement=shift, decrypt_method=direction)
+
+# TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+#Hint: Think about how you can use the modulus (%).
+
+# TODO-3: What happens if the user enters a number/symbol/space?
+# Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+# e.g. start_text = "meet me at 3"
+# end_text = "•••• •• •• 3"
+
+# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
